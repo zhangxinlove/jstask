06c620105c946bc7176dadad0a94a160dec98df4
Neverever <936997223@qq.com>
2018-06-11 15:25:54 +0800

    2018/6/11

---
+++ js1/js1.js
@@ -0,0 +1,37 @@
+console.log("导入JS文件")
+
+var divarr=document.getElementsByClassName("box");
+var int;
+function start(){  
+    clock();//立即执行 
+    clearInterval(int);//防止多次点击开始，闪动加速
+    int =setInterval(clock, 1000);//没1000毫秒执行一次
+}//开始按钮
+
+function bg(){
+    return '#'+Math.floor(Math.random()*0x1000000).toString(16);
+}//随机取颜色
+
+function clock(){
+    for (var i=0;i<divarr.length;i++){
+        divarr[i].style.backgroundColor="orange";
+    }//恢复颜色
+    var x=Math.floor(Math.random()*9);
+    var y=Math.floor(Math.random()*9);
+    var z=Math.floor(Math.random()*9);
+    if (x!=y&&x!=z&&y!=z){
+        divarr[x].style.backgroundColor=bg();
+        divarr[y].style.backgroundColor=bg();
+        divarr[z].style.backgroundColor=bg();
+    }
+    else{
+        clock();
+    }
+}
+
+function stop(){
+    clearInterval(int);
+    for (var i=0;i<divarr.length;i++){
+        divarr[i].style.backgroundColor="orange";
+    }//恢复颜色
+}//停止按钮
