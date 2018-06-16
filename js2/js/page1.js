97ba080515288c4a12cf3bd21aad059c63a401a5
Neverever <936997223@qq.com>
2018-06-16 10:00:55 +0800

    2018/6/16

--- js2/js/page1.js
+++ js2/js/page1.js
@@ -4,8 +4,9 @@ var s=document.getElementById("second");
 var num=5;
 function time(){ 
     document.getElementById("second").innerHTML=num;  
-    if( num===0){
+    if( num<=0){
         location.replace("page2.html");
+        return num=0;
     }
     num=num-1;
 }
