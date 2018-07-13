59152edfcf59cec67a363801a96511b9e8657e60
Neverever <936997223@qq.com>
2018-07-13 17:01:42 +0800

    2018/7/13

--- js5/task5.js
+++ js5/task5.js
@@ -8,19 +8,20 @@ btn.onclick = function () {
     xmlhttp.open('post', '/carrots-admin-ajax/a/login', true); //使用open方法（post类型，地址，异步请求）
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); //添加 HTTP 头
     xmlhttp.send("name=" + user + "&pwd=" + pass); //发送数据到服务器
-    xmlhttp.onreadystatechange = function (data) {//监听事件
+    xmlhttp.onreadystatechange = function (data) { //监听事件
         if (xmlhttp.readyState == 4 //通信成功时，状态为4
-            && xmlhttp.status == 200) {//服务器已成功处理了请求。通常，这表示服务器提供了请求的网页。
+            &&
+            xmlhttp.status == 200) { //服务器已成功处理了请求。通常，这表示服务器提供了请求的网页。
             var data = xmlhttp.responseText;
-            // console.log("data字符串:",data);
-               data = JSON.parse(data);
-               if (data.code == 0) {
-                   location.assign("http://dev.admin.carrots.ptteng.com/#/login");//跳转网页
+            console.log("data字符串:",data);
+            // data = JSON.parse(data);
+            // if (message == success){
+            if (data.code == 0) {
+                // location.assign("http://dev.admin.carrots.ptteng.com/#/login"); //跳转网页
                 // console.log("data:",data);
-               }
-               else {
-                   document.getElementById("hide").innerHTML = data.message;
-               }
+            } else {
+                document.getElementById("hide").innerHTML = data.message;
+            }
         }
     }
 }
